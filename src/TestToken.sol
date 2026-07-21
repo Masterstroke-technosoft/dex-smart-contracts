595aee4b26396aca550ab8ec0c85282c5b239b12
singhritik62 <singhritik62@gmail.com>
2026-07-21 13:22:30 +0530

    add optimised and all the changes from bug bounty

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.6.0
 pragma solidity ^0.8.27;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,11 +9,35 @@ import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20P
 
 contract tMUSD is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
 
+    // Blacklist scheme modeled on Circle's USDC (FiatTokenV2_2 / Blacklistable)
+    address public blacklister;
+    mapping(address => bool) internal _blacklisted;
+
+    event Blacklisted(address indexed account);
+    event UnBlacklisted(address indexed account);
+    event BlacklisterChanged(address indexed newBlacklister);
+
+    error AccountBlacklisted(address account);
+    error CallerNotBlacklister(address caller);
+
+    modifier onlyBlacklister() {
+        if (msg.sender != blacklister) revert CallerNotBlacklister(msg.sender);
+        _;
+    }
+
+    modifier notBlacklisted(address account) {
+        if (_blacklisted[account]) revert AccountBlacklisted(account);
+        _;
+    }
+
     constructor(address initialOwner)
         ERC20("tMUSD Coin", "tMUSDC")
         Ownable(initialOwner)
         ERC20Permit("tMUSDC")
-    {}
+    {
+        blacklister = initialOwner;
+        emit BlacklisterChanged(initialOwner);
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -32,10 +55,41 @@ contract tMUSD is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
         _mint(to, amount);
     }
 
+    function isBlacklisted(address account) external view returns (bool) {
+        return _blacklisted[account];
+    }
+
+    function blacklist(address account) external onlyBlacklister {
+        _blacklisted[account] = true;
+        emit Blacklisted(account);
+    }
+
+    function unBlacklist(address account) external onlyBlacklister {
+        _blacklisted[account] = false;
+        emit UnBlacklisted(account);
+    }
+
+    function updateBlacklister(address newBlacklister) external onlyOwner {
+        require(newBlacklister != address(0), "new blacklister is the zero address");
+        blacklister = newBlacklister;
+        emit BlacklisterChanged(newBlacklister);
+    }
+
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Pausable)
+        notBlacklisted(from)
+        notBlacklisted(to)
     {
         super._update(from, to, value);
+    }
+
+    function _approve(address owner, address spender, uint256 value, bool emitEvent)
+        internal
+        override
+        notBlacklisted(owner)
+        notBlacklisted(spender)
+    {
+        super._approve(owner, spender, value, emitEvent);
     }
 }
